2d92cd0d2179d20d8ff5c26fc6a6e5454978cf4e
Mohammed Raquib <114287448+Mohammed-Raquib@users.noreply.github.com>
2022-10-18 20:26:40 +0530

    header update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ import HeaderSocials from './HeaderSocials';
 const Header = () => {
   return (
     <header>
-      <div id='#' className="container header__container">
+      <div id='/#' className="container header__container">
       <h3>Hello I'm</h3>
       <h1>Mohammed Raquib</h1>
       <h3 className="text-light">Frontend Developer</h3>
